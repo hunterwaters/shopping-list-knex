8db6bb2a767bfa263bccba3c7f65865ff445c1d3
hunterwaters <55896961+hunterwaters@users.noreply.github.com>
2020-03-19 15:39:42 -0700

    Add files via upload

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,23 @@
+INSERT INTO blogful_articles (title, content, date_published)
+VALUES
+    ('Article one', 'This is the content for article one', '2016-01-16' ),
+    ('Article two', 'This is the content for article two', '2016-02-15' ),
+    ('Article three', 'This is the content for article three', '2016-11-12' ),
+    ('Article four', 'This is the content for article four', '2015-02-16' ),
+    ('Article five', 'This is the content for article five', '2001-05-16' ),
+    ('Article six', 'This is the content for article six', '2016-01-17' ),
+    ('Article seven', 'This is the content for article seven', '2002-10-12' ),
+    ('Article eight', 'This is the content for article eight', '2012-09-09' ),
+    ('Article nine', 'This is the content for article nine', '2004-02-12' ),
+    ('Article ten', 'This is the content for article ten', '2003-02-12' ),
+    ('Article eleven', 'This is the content for article eleven', '2016-01-01' ),
+    ('Article twelve', 'This is the content for article twelve', '2010-06-10' ),
+    ('Article thirteen', 'This is the content for article thirteen', '2015-06-18' ),
+    ('Article fourteen', 'This is the content for article fourteen', '2016-01-14' ),
+    ('Article fifteen', 'This is the content for article fifteen', '2015-01-12' ),
+    ('Article sixteen', 'This is the content for article sixteen', '2014-06-06' ),
+    ('Article seventeen', 'This is the content for article seventeen', '2011-11-11' ),
+    ('Article eighteen', 'This is the content for article eighteen', '2007-01-16' ),
+    ('Article nineteen', 'This is the content for article nineteen', '2011-08-18' ),
+    ('Article twenty', 'This is the content for article twenty', '2015-06-16 ')
+;
